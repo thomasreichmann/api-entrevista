b809c663d5ff899e8630e213ed12552f8663c806
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-16 10:44:06 -0300

    endpoint memory_usage

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`App em porta ${port}`))
+
+app.get('/memory_usage', (req, res) => {
+    // Convercao bytes - MB
+    let mem = (process.memoryUsage().rss / 1048576).toFixed(2)
+
+    res.send(mem)
+})
+
+app.get()
