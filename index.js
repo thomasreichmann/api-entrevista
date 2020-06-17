5865c57a883b3b4b8e4bbcedf16138d7d163e273
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-17 09:08:02 -0300

    GET and POST /message

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express()
 const port = 3000
+
+let localMessage = ''
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -13,4 +19,19 @@ app.get('/memory_usage', (req, res) => {
     res.send(mem)
 })
 
-app.get()
+app.get('/message', (req, res) => {
+    if(localMessage) {
+        res.send(localMessage)
+    } else {
+        res.sendStatus(419)
+    }
+})
+
+app.post('/message', (req, res) => {   
+    let newMessage = req.body.newMessage;
+
+    console.log(`Mensagem local: "${localMessage}", Alterada para "${newMessage}" Localmente`)
+
+    localMessage = newMessage
+    res.sendStatus(204)
+})
