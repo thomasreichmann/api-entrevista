a37de58db953e8ca59007e6ee3e06ea1e8db4ee8
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-17 19:22:58 -0300

    live update with socket.io

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
-const express = require('express')
 const bodyParser = require("body-parser");
+
+const express = require('express')
 const app = express()
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
 const port = 3000
 
+const messageEvent = 'newMessage'
 let localMessage = 'Init message'
 
+server.listen(port);
+
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
     extended: false
 }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`App em porta ${port}`))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'))
 
 app.get('/memory_usage', (req, res) => {
-    // Convercao bytes - MB
+    // Convercao bytes -> MB
     let mem = (process.memoryUsage().rss / 1048576).toFixed(2)
 
     res.send(mem)
@@ -26,7 +32,7 @@ app.get('/message', async (req, res) => {
         // Decide se a mensagem e um numero, se sim delay sera o numero, caso a mensagem seja texto, delay sera 0
         let delay = isNaN(localMessage) ? 0 : parseInt(localMessage)
         console.log(`GET /message sendo enviado com ${delay}ms de delay`)
-        // Em teoria essa implementacao mesmo se delay = 0, setTimeout agrega um delay de ate 10ms na execucao
+        // Em teoria nessa implementacao mesmo se delay = 0, setTimeout agregaria um delay de ate 10ms na execucao
         await timeout(delay)
 
         res.send(localMessage)
@@ -41,7 +47,20 @@ app.post('/message', (req, res) => {
     console.log(`Mensagem local: "${localMessage}", Alterada para "${newMessage}" Localmente`)
 
     localMessage = newMessage
+
+    // Apos atualizar localMessage, mandamos a nova localMessage para todas as sockets
+    io.sockets.emit(messageEvent, localMessage)
+
     res.sendStatus(204)
+})
+
+app.get('/live', (req, res) => {
+    // Enviamos a mensagem junto ao nome do socket event que a mensagem sera enviada
+    let data = {
+        message: localMessage,
+        event: messageEvent
+    }
+    res.send(data)
 })
 
 function timeout(ms) {
