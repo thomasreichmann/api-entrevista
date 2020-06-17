e453cade0b9ae0fedb8ec0aa00fc4375eaf022c4
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-17 19:22:33 -0300

    mockup frontend for testing

---
+++ public/index.js
@@ -0,0 +1,22 @@
+const socket = io.connect('http://localhost:3000');
+
+$.get("live", (requestData) => {
+    $('#getlive').text(requestData.message)
+
+    socket.on(requestData.event, (socketData) => {
+        $('#live').text(socketData)
+    })  
+})
+
+$(document).ready(function () {
+    $('#btGetMessage').click(() => {
+        console.log(`a`)
+        $.get("message", (data) => {
+            $('#get').text(data)
+        })
+    })
+
+    $('#btPostMessage').click(() => {
+        $.post("message", {newMessage: $("#postMessageInput").val()})
+    })
+})
