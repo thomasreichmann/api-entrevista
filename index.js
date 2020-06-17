04bc1bc73e05a1877a8f659a7b51d82c9bac0259
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-17 09:26:03 -0300

    Added delay to GET /message if stored <string> if string is a number

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const bodyParser = require("body-parser");
 const app = express()
 const port = 3000
 
-let localMessage = ''
+let localMessage = 'Init message'
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => res.send('Hello World!'))
@@ -19,15 +21,21 @@ app.get('/memory_usage', (req, res) => {
     res.send(mem)
 })
 
-app.get('/message', (req, res) => {
-    if(localMessage) {
+app.get('/message', async (req, res) => {
+    if (localMessage) {
+        // Decide se a mensagem e um numero, se sim delay sera o numero, caso a mensagem seja texto, delay sera 0
+        let delay = isNaN(localMessage) ? 0 : parseInt(localMessage)
+        console.log(`GET /message sendo enviado com ${delay}ms de delay`)
+        // Em teoria essa implementacao mesmo se delay = 0, setTimeout agrega um delay de ate 10ms na execucao
+        await timeout(delay)
+
         res.send(localMessage)
     } else {
         res.sendStatus(419)
     }
 })
 
-app.post('/message', (req, res) => {   
+app.post('/message', (req, res) => {
     let newMessage = req.body.newMessage;
 
     console.log(`Mensagem local: "${localMessage}", Alterada para "${newMessage}" Localmente`)
@@ -35,3 +43,7 @@ app.post('/message', (req, res) => {
     localMessage = newMessage
     res.sendStatus(204)
 })
+
+function timeout(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
