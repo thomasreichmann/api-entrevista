77d8609f1defaed072aeea37d1b91be9222cc5b9
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-19 01:21:27 -0300

    changed port to 80

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express()
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
-const port = 3000
+const port = 80
 
 const messageEvent = 'newMessage'
 let localMessage = 'Init message'
