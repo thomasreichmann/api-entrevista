eb7076a468c4ba5583b507c50fef4ea83b13d0c0
Thomas Reichmann <thomasreichmann@hotmail.com>
2020-06-19 02:04:08 -0300

    added comments

--- public/index.js
+++ public/index.js
@@ -1,5 +1,8 @@
+// IP e port precisam ser alterados para os da maquina hosteando a api quando em "producao"
+// caso contrario, localhost e mais pratico para desenvolvimento
 const socket = io.connect('http://localhost:80');
 
+// GET /live recebe o eventname e prepara o socket para receber o evento
 $.get("live", (requestData) => {
     $('#getlive').text(requestData.message)
 
@@ -9,13 +12,15 @@ $.get("live", (requestData) => {
 })
 
 $(document).ready(function () {
+
+    // GET /message e atualiza o <p> do site
     $('#btGetMessage').click(() => {
-        console.log(`a`)
         $.get("message", (data) => {
             $('#get').text(data)
         })
     })
 
+    // POST /message com o valor do <input>
     $('#btPostMessage').click(() => {
         $.post("message", {newMessage: $("#postMessageInput").val()})
     })
